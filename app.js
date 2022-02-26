cb24f94fb1996818e052bdc8b28b8350ce9611e2
Chase Newman <chasejn01@gmail.com>
2022-02-26 08:46:40 -0500

    complete project

--- app.js
+++ app.js
@@ -1,38 +1,32 @@
 //This first prompt sets userInput
 let userInput = prompt("What would you like to do?");
-let todoArr = [];
+const todoArr = [];
 
 //userInput is then passed through this while loop
 //checking it against the conditionals
 while(userInput !== "quit") {
     if(userInput === "new") {
-        let newTodo = prompt("Enter a new todo");
+        const newTodo = prompt("Enter a new todo");
         todoArr.push(newTodo);
-        userInput = prompt("What would you like to do next?");
     }
     else if(userInput === "list") {
         for(let i=0; i<todoArr.length; i++) {
             console.log(`${i}: ${todoArr[i]}`);
         }
         console.log("*************");
-        userInput = prompt("What would you like to do next?");
     }
     else if(userInput === "delete") {
-        console.log("delete");
-        num = parseInt(prompt("Which todo would you like to delete? (please enter an num)"));
-        // if(num !== NaN) {
-        //     todoArr.slice(num);    
-        // }
+        let num = parseInt(prompt("Which todo would you like to delete? (please enter an index)"));
         if(num >= 0 && num < todoArr.length) {
-            console.log(todoArr.slice(num));
-            todoArr = todoArr.slice(num);
+            const deleted = todoArr.splice(num, 1);
+            console.log(`Ok, you deleted ${deleted}`);
         }
         else {
-            num = parseInt(prompt("That was not a valid number. Please try again"));
+          console.log("Invalid index. Please enter a new command");
         }
-        userInput = prompt("What would you like to do next?");
     }
     else {
         userInput = prompt("That was not a valid command. Please try again");
     }
+    userInput = prompt("What would you like to do next?");
 }
