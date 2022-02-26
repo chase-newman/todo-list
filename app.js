5ceb4593d2d926ffae935e855cfa1c92c7e3211e
Chase Newman <chasejn01@gmail.com>
2022-02-26 07:47:45 -0500

    add delete function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //This first prompt sets userInput
 let userInput = prompt("What would you like to do?");
-const todoArr = [];
+let todoArr = [];
 
 //userInput is then passed through this while loop
 //checking it against the conditionals
@@ -19,9 +19,20 @@ while(userInput !== "quit") {
     }
     else if(userInput === "delete") {
         console.log("delete");
+        num = parseInt(prompt("Which todo would you like to delete? (please enter an num)"));
+        // if(num !== NaN) {
+        //     todoArr.slice(num);    
+        // }
+        if(num >= 0 && num < todoArr.length) {
+            console.log(todoArr.slice(num));
+            todoArr = todoArr.slice(num);
+        }
+        else {
+            num = parseInt(prompt("That was not a valid number. Please try again"));
+        }
         userInput = prompt("What would you like to do next?");
     }
     else {
-        userInput = promt("That was not a valid command. Please try again");
+        userInput = prompt("That was not a valid command. Please try again");
     }
 }
