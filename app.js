f0bc4826d85eaf283ffff66f3a4516e3caf34b47
Chase Newman <chasejn01@gmail.com>
2022-02-26 07:01:23 -0500

    add infinite loop (whoops)

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("Todo List");
+//This first prompt sets userInput
+let userInput = prompt("What would you like to do?");
+const todoArr = [];
+
+console.log(userInput);
+//userInput is then passed through this while loop
+//checking it against the conditionals
+while(userInput !== "quit") {
+    // userInput = prompt("What would you like to do?");
+    
+    if(userInput === "new") {
+        console.log("new")
+        // todoArr.push(userInput);
+    }
+    if(userInput === "list") {
+        // console.log(todoArr);
+        console.log("list");
+    }
+    if(userInput === "delete") {
+        console.log("delete");
+    }
+}
